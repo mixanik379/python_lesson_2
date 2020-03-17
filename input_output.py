a899f26c0a56022dd8fecae7d926f3f0c3476eba
mixanik379 <zqy@bk.ru>
2020-03-17 20:52:16 +0300

    для push

---
+++ input_output.py
@@ -0,0 +1,22 @@
+# однострочный комментарий
+
+'''
+
+первая строка
+вторая строка
+
+
+'''
+
+print('Hello!')
+print('Hello!', 'student!', 123, sep='xxx')
+
+print('Hello!', 'student!', 123, end='yyy')
+
+print()
+# Ввод
+
+age = input('Input your age')
+
+print(age, type(age))
+print(int(age), type(int(age)))
---
+++ math_operations.py
@@ -0,0 +1,38 @@
+'''
+a = input('First number')
+b = input('Second number')
+
+a = int (a)
+b = int (b)
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+result = a / b
+print(type(result))
+print(result)
+
+print(a ** 2)
+
+'''
+# Логические операции
+a = True
+b = False
+
+# Отрицание
+print(not a)
+
+# Логическое И
+print(a and b)
+
+# Логическое ИЛИ
+print(a or b)
+
+a = 10
+print(a > 100)
+print(a < 100)
+print(a <= 100)
+print(a >= 100)
+print(a == 100)
+print(a != 100)
